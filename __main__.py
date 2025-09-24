32fd9b596aa55924917c9efdaedbbc88aba7acae
de4ps <97440587+de4ps@users.noreply.github.com>
2025-09-24 08:52:58 +0000

    feat: add create repo

--- __main__.py
+++ __main__.py
@@ -1,3 +1,29 @@
 """A Python Pulumi program"""
 
 import pulumi
+import pulumi_github as github
+
+
+docker_template = {
+    "owner": "haqops",
+    "repository": "secure-mcp-server",
+    "include_all_branches": False,
+}
+
+mcps = {
+    "yahoofinance2": {
+        "description": "Yahoo Finance MCP",
+        "github_template": docker_template,
+    }
+}
+
+
+for key, value in mcps.items():
+    repo_name = f"{key}-mcp"
+    # https://www.pulumi.com/registry/packages/github/api-docs/repository/
+    repo = github.Repository(key,
+        name=repo_name,
+        description=value["description"],
+        visibility="public",
+        template=value["github_template"]
+    )
